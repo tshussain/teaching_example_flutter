bdd406e7cff9777bf883bd94985b6554e3492d49
Talib Hussain <thussain@users.noreply.github.com>
2023-06-24 16:38:31 -0400

    testing git

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ void main() async {
   runApp(const MyApp());
 }
 
+// Adding documentation
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   // This widget is the root of your application.
