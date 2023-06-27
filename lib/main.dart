fdc0d7d0557cd3722b4292ae6068c8e330e79561
Talib Hussain <thussain@users.noreply.github.com>
2023-06-27 11:31:58 -0400

    2.1 docs

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ void main() async {
   runApp(const MyApp());
 }
 
-// This is my cool Branch 2 documentation
+// This is my cool Branch 2.1 documentation
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   // This widget is the root of your application.
